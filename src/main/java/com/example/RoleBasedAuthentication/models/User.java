a8ae5d24f8378007039ce564a08df310fe389497
Krityan Kashyap <krityankashyap@Krityans-MacBook-Air.local>
2026-06-12 16:46:30 +0530

    Added UserDetails

--- src/main/java/com/example/RoleBasedAuthentication/models/User.java
+++ src/main/java/com/example/RoleBasedAuthentication/models/User.java
@@ -1,5 +1,12 @@
 package com.example.RoleBasedAuthentication.models;
 
+import java.util.Collection;
+import java.util.Collections;
+
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.EnumType;
@@ -14,7 +21,7 @@ import lombok.Setter;
 @Setter
 @RequiredArgsConstructor
 @Table(name = "users")
-public class User extends BaseEntity {
+public class User extends BaseEntity implements UserDetails{
 
    @Column(name = "username", nullable = false)
     private String displayName;
@@ -28,4 +35,14 @@ public class User extends BaseEntity {
     @Enumerated(EnumType.STRING)
     @Column(nullable = false)
     private Roles role;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {  // This method is used to get the authorities (roles) of the user. It returns a collection of GrantedAuthority objects, which represent the roles assigned to the user. In this implementation, it creates a SimpleGrantedAuthority with the role name prefixed by "ROLE_" and returns it as a singleton list.
+      return Collections.singletonList(new SimpleGrantedAuthority("ROLE_" + role.name()));
+    }
+
+    @Override
+    public String getUsername(){ // This method is used to get the username of the user. In this implementation, it returns the email as the username, which is a common practice in many applications where the email serves as the unique identifier for the user.
+      return email;
+    }
 }
